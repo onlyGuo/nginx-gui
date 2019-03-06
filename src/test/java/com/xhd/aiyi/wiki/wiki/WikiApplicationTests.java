6d272d0950ef6a5bb121bfdce4d9d657d55c7643
climba03003 <climba03003@gmail.com>
2019-03-06 17:06:15 +0800

    Fix Error when using Maven 3.6 #1 #5

--- src/test/java/com/xhd/aiyi/wiki/wiki/WikiApplicationTests.java
+++ src/test/java/com/xhd/aiyi/wiki/wiki/WikiApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = com.aiyi.server.manager.nginx.NginxManagerApplication.class)
 public class WikiApplicationTests {
 
 	@Test
