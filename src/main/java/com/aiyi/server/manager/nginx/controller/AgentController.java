cde1800bab68d116ec856bc63e6e3733cface215
gsk <guosk@lianjintai.com>
2019-04-18 15:26:02 +0800

    Fix: 解决upstream节点位置问题

--- src/main/java/com/aiyi/server/manager/nginx/controller/AgentController.java
+++ src/main/java/com/aiyi/server/manager/nginx/controller/AgentController.java
@@ -39,211 +39,284 @@ import com.github.odiszapc.nginxparser.NgxToken;
 
 /**
  * 反向代理控制台
+ *
  * @Project : nginx-gui
  * @Program Name : com.aiyi.server.manager.nginx.controller.AgentController.java
- * @Description : 
+ * @Description :
  * @Author : 郭胜凯
  * @Creation Date : 2018年2月22日 下午5:44:38
  * @ModificationHistory Who When What ---------- ------------- -----------------------------------
- *                      郭胜凯 2018年2月22日 create
+ * 郭胜凯 2018年2月22日 create
  */
 @Controller
 @RequestMapping("admin/agent")
 public class AgentController {
 
-  @Resource
-  private NginxManager nginxManager;
-  
-  /**
-   * 反向代理管理页面
-   * @Description : 
-   * @return : String
-   * @Creation Date : 2018年2月7日 下午5:01:43
-   * @Author : 郭胜凯
-   */
-  @RequestMapping("back")
-  public String back() {
-    return "admin/agent/back/index";
-  }
-  
-  /**
-   * 
-   * @Description : 
-   * @return : String
-   * @throws IOException 
-   * @Creation Date : 2018年2月22日 下午5:42:46
-   * @Author : 郭胜凯
-   */
-  @RequestMapping("back/edit/{value}")
-  public String edit(@PathVariable String value, Model model) throws IOException {
-	  if(!Vali.isEpt(value)) {
-		  for (NginxUpstream nginxUpstream : listUpstreams(NginxUtils.read())) {
-			if (!Vali.isEpt(nginxUpstream.getValue()) && value.equals(nginxUpstream.getValue())) {
-				model.addAttribute("upstream", nginxUpstream);
-				break;
-			}
-		  }
-	  }
-	  return "admin/agent/back/edit";
-  }
-  
-  /**
-   * 保存负载配置
-   * @Description : 
-   * @return : Result
-   * @Creation Date : 2018年2月23日 下午6:56:00
-   * @Author : 郭胜凯
-   */
-  @RequestMapping(value = "back/save", method = RequestMethod.PUT)
-  @ResponseBody
-  public Result save(@RequestBody NginxUpstream upstream) {
-	  if (Vali.isEpt(upstream)) {
-		  throw new ValidationException("请输入正确的配置");
-	  }
-	  if (Vali.isEpt(upstream.getDesp()) || Vali.isEpt(upstream.getValue())) {
-		  throw new ValidationException("负载描述和负载名称不能为空");
-	  }
-	  if(upstream.getItemsLength() < 1) {
-		  throw new ValidationException("至少要配置一个节点地址");
-	  }
-	  Result result = new Result();
-	  result.setCode(CommonFields.ERROR_CODE.SERVER);
-	  result.setMessage("未找到目标配置信息");
-	  NgxConfig conf = NginxUtils.read();
-	  String backConf = NginxUtils.toString(conf);
-	  NgxEntry old = null;
-	  List<NgxEntry> findAll = conf.findAll(NgxConfig.BLOCK, "http", "upstream");
-	    for (NgxEntry ngxEntry : findAll) {
-	    		//定位负载配置
-	    		if (upstream.getValue().equals(((NgxBlock)ngxEntry).getValue())) {
-	    			old = ngxEntry;
-	    			break;
-	    		}
-	    		
-		}
-	    //更新Conf
-	    addUpstream(conf, upstream, old);
-	    //尝试写到Nginx配置文件
-		try {
-			NginxUtils.save(conf);
-			//重启Nginx
-			nginxManager.reload();
-			result.setCode("SUCCESS");
-			result.setSuccess(true);
-		} catch (Exception e) {
-			//恢复到上一次配置
-			NginxUtils.save(backConf);
-			throw new ValidationException("已恢复到上一次配置:" + e.getMessage());
-		}
-		return result;
-  }
-  
-  @RequestMapping(value = "back/del/{value}", method = RequestMethod.PUT)
-  @ResponseBody
-  public String del(@PathVariable String value) {
-	if (Vali.isEpt(value)) {
-		throw new ValidationException("不能传递空的upstream值");
-	}
-	NgxConfig conf = NginxUtils.read();
-	  String backConf = NginxUtils.toString(conf);
-	  List<NgxEntry> findAll = conf.findAll(NgxConfig.BLOCK, "http", "upstream");
-	    for (NgxEntry ngxEntry : findAll) {
-	    		//定位负载配置
-	    		if (value.equals(((NgxBlock)ngxEntry).getValue())) {
-	    			conf.remove(ngxEntry);
-	    			break;
-	    		}
-		}
-	    //尝试写到Nginx配置文件
-		try {
-			NginxUtils.save(conf);
-			//重启Nginx
-			nginxManager.reload();
-		} catch (Exception e) {
-			//恢复到上一次配置
-			NginxUtils.save(backConf);
-			throw new ValidationException("已恢复到上一次配置:" + e.getMessage());
-		}
-	return "success";
-  }
-  
-  /**
-   * 获取反向代理配置列表的前段接收Json
-   * @Description : 
-   * @return : List<NginxConfig>
-   * @throws IOException 
-   * @Creation Date : 2018年2月7日 下午5:06:08
-   * @Author : 郭胜凯
-   */
-  @RequestMapping("back/list")
-  @ResponseBody
-  public TableDate listBack(int start, int length) throws IOException{
-    TableDate tableDate = new TableDate();
-    List<NginxUpstream> result = listUpstreams(NginxUtils.read());
-    tableDate.setList(result);
-    return tableDate;
-  }
-  
-  private void addUpstream(NgxConfig conf, NginxUpstream upstream, NgxEntry ngxEntry) {
-	NgxBlock newBlock = new NgxBlock();
-	newBlock.addEntry(new NgxComment(" " + upstream.getDesp()));
-	for (NginxUpstreamItem item : upstream.getItems()) {
-		NgxParam param = new NgxParam();
-		param.addValue("server " + item.getAddress());
-		param.addValue("weight=" + item.getWeight());
-		newBlock.addEntry(param);
-	}
-	newBlock.addValue(new NgxToken(upstream.getName()));
-	newBlock.addValue(new NgxToken(upstream.getValue()));
-	
-	NgxBlock findBlock = conf.findBlock("http");
-	conf.remove(findBlock);
-	if (null != ngxEntry) {
-		findBlock.remove(ngxEntry);
-	}
-	findBlock.addEntry(newBlock);
-	conf.addEntry(findBlock);
-	
-  }
-  
-  /**
-   * 获得反向代理配置列表
-   * @Description : 
-   * @return : List<NginxUpstream>
-   * @Creation Date : 2018年2月22日 下午6:27:32
-   * @Author : 郭胜凯
-   */
-  public List<NginxUpstream> listUpstreams(NgxConfig conf){
-	    List<NgxEntry> findAll = conf.findAll(NgxConfig.BLOCK, "http", "upstream");
-	    List<NginxUpstream> result = new ArrayList<>();
-	    for (NgxEntry ngxEntry : findAll) {
-		    	NginxUpstream upstream = new NginxUpstream();
-		    	upstream.setItems(new ArrayList<>());
-	    		upstream.setName(((NgxBlock)ngxEntry).getName());
-	    		String value = ((NgxBlock)ngxEntry).getValue();
-	    		upstream.setValue(value);
-	    		((NgxBlock)ngxEntry).forEach((a) ->{
-	    			if(a instanceof NgxComment) {
-	    				upstream.setDesp(((NgxComment) a).getValue());
-	    			}else {
-	    				NgxParam param = (NgxParam) a;
-	    				NginxUpstreamItem item = new NginxUpstreamItem();
-	    				List<String> values = param.getValues();
-	    				item.setAddress(values.get(0));
-	    				if (values.size() > 1) {
-	    					item.setWeight(Integer.parseInt(values.get(1).replace("weight=", "").trim()));
-					}
-	    				upstream.getItems().add(item);
-	    			}
-	    		});
-	    		if (null == upstream.getDesp()) {
-	    			upstream.setDesp(UUID.randomUUID().toString().replace("-", ""));
-	    			((NgxBlock)ngxEntry).addEntry(new NgxComment(upstream.getDesp()));
-	    			NginxUtils.save(conf);
-			}
-	    		result.add(upstream);
-	    		System.out.println(upstream);
-		}
-	    return result;
-  }
+    @Resource
+    private NginxManager nginxManager;
+
+    /**
+     * 反向代理管理页面
+     *
+     * @return : String
+     * @Description :
+     * @Creation Date : 2018年2月7日 下午5:01:43
+     * @Author : 郭胜凯
+     */
+    @RequestMapping("back")
+    public String back() {
+        return "admin/agent/back/index";
+    }
+
+    /**
+     * @return : String
+     * @throws IOException
+     * @Description :
+     * @Creation Date : 2018年2月22日 下午5:42:46
+     * @Author : 郭胜凯
+     */
+    @RequestMapping("back/edit/{value}")
+    public String edit(@PathVariable String value, Model model) throws IOException {
+        if (!Vali.isEpt(value)) {
+            for (NginxUpstream nginxUpstream : listUpstreams(NginxUtils.read())) {
+                if (!Vali.isEpt(nginxUpstream.getValue()) && value.equals(nginxUpstream.getValue())) {
+                    model.addAttribute("upstream", nginxUpstream);
+                    break;
+                }
+            }
+        }
+        return "admin/agent/back/edit";
+    }
+
+    /**
+     * 保存负载配置
+     *
+     * @return : Result
+     * @Description :
+     * @Creation Date : 2018年2月23日 下午6:56:00
+     * @Author : 郭胜凯
+     */
+    @RequestMapping(value = "back/save", method = RequestMethod.PUT)
+    @ResponseBody
+    public Result save(@RequestBody NginxUpstream upstream) {
+        if (Vali.isEpt(upstream)) {
+            throw new ValidationException("请输入正确的配置");
+        }
+        if (Vali.isEpt(upstream.getDesp()) || Vali.isEpt(upstream.getValue())) {
+            throw new ValidationException("负载描述和负载名称不能为空");
+        }
+        if (upstream.getItemsLength() < 1) {
+            throw new ValidationException("至少要配置一个节点地址");
+        }
+        Result result = new Result();
+        result.setCode(CommonFields.ERROR_CODE.SERVER);
+        result.setMessage("未找到目标配置信息");
+        NgxConfig conf = NginxUtils.read();
+        String backConf = NginxUtils.toString(conf);
+        NgxEntry old = null;
+        List<NgxEntry> findAll = conf.findAll(NgxConfig.BLOCK, "http", "upstream");
+        for (NgxEntry ngxEntry : findAll) {
+            //定位负载配置
+            if (upstream.getValue().equals(((NgxBlock) ngxEntry).getValue())) {
+                old = ngxEntry;
+                break;
+            }
+
+        }
+        //更新Conf
+        addUpstream(conf, upstream, old);
+        //尝试写到Nginx配置文件
+        try {
+            NginxUtils.save(conf);
+            //重启Nginx
+            nginxManager.reload();
+            result.setCode("SUCCESS");
+            result.setSuccess(true);
+        } catch (Exception e) {
+            //恢复到上一次配置
+            NginxUtils.save(backConf);
+            throw new ValidationException("已恢复到上一次配置:" + e.getMessage());
+        }
+        return result;
+    }
+
+    @RequestMapping(value = "back/del/{value}", method = RequestMethod.PUT)
+    @ResponseBody
+    public String del(@PathVariable String value) {
+        if (Vali.isEpt(value)) {
+            throw new ValidationException("不能传递空的upstream值");
+        }
+        NgxConfig conf = NginxUtils.read();
+        String backConf = NginxUtils.toString(conf);
+        List<NgxEntry> findAll = conf.findAll(NgxConfig.BLOCK, "http", "upstream");
+        for (NgxEntry ngxEntry : findAll) {
+            //定位负载配置
+            if (value.equals(((NgxBlock) ngxEntry).getValue())) {
+                conf.remove(ngxEntry);
+                break;
+            }
+        }
+        //尝试写到Nginx配置文件
+        try {
+            NginxUtils.save(conf);
+            //重启Nginx
+            nginxManager.reload();
+        } catch (Exception e) {
+            //恢复到上一次配置
+            NginxUtils.save(backConf);
+            throw new ValidationException("已恢复到上一次配置:" + e.getMessage());
+        }
+        return "success";
+    }
+
+    /**
+     * 获取反向代理配置列表的前段接收Json
+     *
+     * @return : List<NginxConfig>
+     * @throws IOException
+     * @Description :
+     * @Creation Date : 2018年2月7日 下午5:06:08
+     * @Author : 郭胜凯
+     */
+    @RequestMapping("back/list")
+    @ResponseBody
+    public TableDate listBack(int start, int length) throws IOException {
+        TableDate tableDate = new TableDate();
+        List<NginxUpstream> result = listUpstreams(NginxUtils.read());
+        tableDate.setList(result);
+        return tableDate;
+    }
+
+    private void addUpstream(NgxConfig conf, NginxUpstream upstream, NgxEntry ngxEntry) {
+
+        NgxBlock httpBlock = conf.findBlock("http");
+        NgxBlock newHttpBlock = new NgxBlock();
+        newHttpBlock.addValue("http");
+
+        List<NgxEntry> entryList = new ArrayList<>(httpBlock.getEntries());
+        NgxBlock newBlock = new NgxBlock();
+        newBlock.addValue(new NgxToken(upstream.getName()));
+        newBlock.addValue(new NgxToken(upstream.getValue()));
+        boolean isInsert = true;
+        for (NgxEntry entry : entryList) {
+            newHttpBlock.addEntry(entry);
+            if (!(entry instanceof NgxBlock)){
+                continue;
+            }
+            NgxBlock block = (NgxBlock) entry;
+            if (!"upstream".equalsIgnoreCase(block.getName())){
+                continue;
+            }
+            if (!upstream.getValue().equals(block.getValue())){
+                continue;
+            }
+            // 找到我要改的upstream块
+            isInsert = false;
+            newHttpBlock.remove(entry);
+            List<NgxEntry> upstreamEnrties = new ArrayList<>(block.getEntries());
+
+            // 更改注释
+            if (upstreamEnrties.size() > 0 && upstreamEnrties.get(0) instanceof NgxComment){
+                upstreamEnrties.remove(0);
+            }
+            newBlock.addEntry(new NgxComment("#" + upstream.getDesp()));
+
+            // 负载列表
+            for (NginxUpstreamItem item : upstream.getItems()) {
+                NgxParam param = new NgxParam();
+                param.addValue("server " + item.getAddress());
+                param.addValue("weight=" + item.getWeight());
+                newBlock.addEntry(param);
+            }
+
+            // 其他
+            for (NgxEntry entryItem: upstreamEnrties){
+                if (!(entryItem instanceof NgxParam)){
+                    newBlock.addEntry(entryItem);
+                    continue;
+                }
+                NgxParam upstreamItem = (NgxParam) entryItem;
+                if (!upstreamItem.getName().equals("server")){
+                    newBlock.addEntry(upstreamItem);
+                }
+            }
+            newHttpBlock.addEntry(newBlock);
+        }
+
+        if (isInsert){
+            newBlock.addEntry(new NgxComment(" " + upstream.getDesp()));
+            for (NginxUpstreamItem item : upstream.getItems()) {
+                NgxParam param = new NgxParam();
+                param.addValue("server " + item.getAddress());
+                param.addValue("weight=" + item.getWeight());
+                newBlock.addEntry(param);
+            }
+            // 在第一个server节点前面添加集群
+            newHttpBlock = new NgxBlock();
+            newHttpBlock.addValue("http");
+
+            for (NgxEntry entry : entryList) {
+                newHttpBlock.addEntry(entry);
+                if (!(entry instanceof NgxBlock)) {
+                    continue;
+                }
+                NgxBlock block = (NgxBlock) entry;
+                if (!"server".equalsIgnoreCase(block.getName())) {
+                    continue;
+                }
+                newHttpBlock.remove(entry);
+                newHttpBlock.addEntry(newBlock);
+                newHttpBlock.addEntry(entry);
+                isInsert = false;
+            }
+            if (isInsert){
+                newHttpBlock.addEntry(newBlock);
+            }
+        }
+
+        conf.remove(httpBlock);
+        conf.addEntry(newHttpBlock);
+    }
+
+    /**
+     * 获得反向代理配置列表
+     *
+     * @return : List<NginxUpstream>
+     * @Description :
+     * @Creation Date : 2018年2月22日 下午6:27:32
+     * @Author : 郭胜凯
+     */
+    public List<NginxUpstream> listUpstreams(NgxConfig conf) {
+        List<NgxEntry> findAll = conf.findAll(NgxConfig.BLOCK, "http", "upstream");
+        List<NginxUpstream> result = new ArrayList<>();
+        for (NgxEntry ngxEntry : findAll) {
+            NginxUpstream upstream = new NginxUpstream();
+            upstream.setItems(new ArrayList<>());
+            upstream.setName(((NgxBlock) ngxEntry).getName());
+            String value = ((NgxBlock) ngxEntry).getValue();
+            upstream.setValue(value);
+            ((NgxBlock) ngxEntry).forEach((a) -> {
+                if (a instanceof NgxComment) {
+                    upstream.setDesp(((NgxComment) a).getValue());
+                } else {
+                    NgxParam param = (NgxParam) a;
+                    NginxUpstreamItem item = new NginxUpstreamItem();
+                    List<String> values = param.getValues();
+                    item.setAddress(values.get(0));
+                    if (values.size() > 1) {
+                        item.setWeight(Integer.parseInt(values.get(1).replace("weight=", "").trim()));
+                    }
+                    upstream.getItems().add(item);
+                }
+            });
+            if (null == upstream.getDesp()) {
+                upstream.setDesp(UUID.randomUUID().toString().replace("-", ""));
+                ((NgxBlock) ngxEntry).addEntry(new NgxComment(upstream.getDesp()));
+                NginxUtils.save(conf);
+            }
+            result.add(upstream);
+            System.out.println(upstream);
+        }
+        return result;
+    }
 }
